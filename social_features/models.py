0179454c85016442ba5b70acc6a3d46813dd99c2
Anit Paul <anit42655@gmail.com>
2025-05-27 21:08:36 +0530

    Post model added

--- social_features/models.py
+++ social_features/models.py
@@ -11,3 +11,18 @@ class Follow(models.Model):
 
     def __str__(self):
         return f"{self.follower.username} follows {self.following.username}"
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name="posts",  # use 'posts' not 'following'
+        on_delete=models.CASCADE
+    )
+    caption = models.TextField(blank=True)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # set this automatically
+    likes = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, 
+        related_name='liked_posts', 
+        blank=True
+    )
