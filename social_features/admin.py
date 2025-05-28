f06343cf11d8b2a19ea59178a00eda17735c1003
Anit Paul <anit42655@gmail.com>
2025-05-28 22:14:53 +0530

    some changes done

--- social_features/admin.py
+++ social_features/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+<<<<<<< HEAD
 from .models import Post,Follow
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Follow)
+=======
+
+# Register your models here.
+>>>>>>> 53196f0 (social features app added)
